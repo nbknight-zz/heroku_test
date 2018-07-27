813ebd444f01c5892eeed074b4571fdf2e577bbf
Nick Knight <nicholas.b.knight@gmail.com>
2018-07-27 10:56:53 -0400

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,19 @@
+# CarrierWave.configure do |config|
+# 	config.fog_credentials = {
+# 		:provider => "AWS",
+# 		:aws_access_key_id => "#{ENV['AWS_ACCESS_KEY_ID']}",
+# 		:aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
+# 		:region => "#{ENV['S3_REGION']}",
+# 		:path_style => :true
+# 	}
+
+# 	config.fog_directory = "#{ENV['S3_BUCKET_NAME']}"
+# 	confg.cache_dir = "#{Rails.root}/tmp/uploads"
+	
+# end
 CarrierWave.configure do |config|
 	config.fog_credentials = {
-		:provider => "AWS",
+		:provider => 'AWS',
 		:aws_access_key_id => "#{ENV['AWS_ACCESS_KEY_ID']}",
 		:aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
 		:region => "#{ENV['S3_REGION']}",
@@ -8,6 +21,6 @@ CarrierWave.configure do |config|
 	}
 
 	config.fog_directory = "#{ENV['S3_BUCKET_NAME']}"
-	confg.cache_dir = "#{Rails.root}/tmp/uploads"
-	
+	config.cache_dir = "#{Rails.root}/tmp/uploads"
+
 end
